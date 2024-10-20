575b02296eeb454af6f96d67dc08fe7bb201ceb7
AhsanKhan <ahsankhan.ubit>
2024-10-20 20:49:18 +0500

    Created Build For Production Temprary Disable Build

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ app.use('/api/places', require('./routes/places'));
 
 // Swagger setup
 //swaggerSetup(app);
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 // port
 const PORT = process.env.PORT || 8080;
